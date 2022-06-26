0d4aac79c2007eff1cc0aded50794d51f6f73e07
mutemip <paulmutemi2030@gmail.com>
2022-06-26 19:50:27 +0300

    a simple todo App test done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    populateToDo(e.target.new_task_description.value);
+    //reset form data
+    form.reset();
+  });
 });
+
+
+function populateToDo(task){
+  //create DOM element for our tasks
+  let li = document.createElement('li');
+  let btn = document.createElement('button')
+  //delete event
+  btn.addEventListener('click', handleDelete)
+  li.textContent = `${task} `;
+  btn.textContent = 'Delete'
+  li.appendChild(btn)
+  //append data to parent
+  document.querySelector('#tasks').appendChild(li)
+
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
